9d349a71adccb248ab7a042dbbf1adefd3d6869e
Jorge Dominguez <jorgebee65@hotmail.com>
2018-08-06 22:32:13 -0500

    Formulario para agregar Taqueria

--- vuejs-md/src/components/Dashboard.vue
+++ vuejs-md/src/components/Dashboard.vue
@@ -1,13 +1,58 @@
 <template>
 	<div id="dashboard">
 		<h3>Dashboard</h3>
+		<div  >
+          <md-card md-with-hover v-for="taq in taquerias">
+          	<md-card-media>
+          		<img :src="getImageUrl(taq.id)" />
+          	</md-card-media>
+		      <md-card-header>
+		        <div class="md-title">{{taq.nombre}}</div>
+		      </md-card-header>
+
+		      <md-card-content>
+		     {{taq.ubicacion}}
+		      </md-card-content>
+		      <md-card-actions>
+	            	<md-button v-bind:to="{name: 'view-element', params:{taq_id: taq.id}}">MAS</md-button>
+	          </md-card-actions>
+		   </md-card>
+
+        </div>
 	</div>
 </template>
 <script>
+	import axios from 'axios'
+	const URL = "http://localhost:8080/tacofy/taquerias"
 	export default{
-		name:'dashboard',
+		name: 'dashboard',
 		data(){
-			return {}
-		}
+			return{
+				taquerias:[]
+			}
+		},
+	  mounted: function(){
+	    axios.get(URL).then(response => {
+	    	this.taquerias = response.data	
+	    })
+	    $('.parallax').parallax()
+	  },
+	  methods:{
+	    getImageUrl: function(id){
+	      return "http://localhost:8080/tacofy/taquerias/"+id+"/image";
+	    }
+	  }
 	}
 </script>
+<style lang="scss" scoped>
+  .card-expansion {
+    height: 480px;
+  }
+
+  .md-card {
+    width: 320px;
+    margin: 4px;
+    display: inline-block;
+    vertical-align: top;
+  }
+</style>
--- vuejs-md/src/components/NewElement.vue
+++ vuejs-md/src/components/NewElement.vue
@@ -1,25 +1,146 @@
 <template>
 	<div id="new-element">
-		<h3>New-element</h3>
+		 <md-steppers :md-active-step.sync="active" md-vertical md-linear>
+		 	<md-step id="first" md-label="First Step" md-description="Optional" :md-editable="false" :md-done.sync="first">
+		 		<div class="viewport">
+				<md-toolbar :md-elevation="1">
+			      <span class="md-title">Registra</span>
+			    </md-toolbar>
+			    <md-list class="md-double-line">
+			    	<md-subheader>Información General</md-subheader>
+			    	<md-list-item>
+				        <md-icon class="md-primary">store</md-icon>
+
+				        <md-field>
+					      <label>Nombre</label>
+					      <md-input v-model="name"></md-input>
+					    </md-field>
+
+			        </md-list-item>
+			        <md-list-item>
+				        <md-icon class="md-primary">phone</md-icon>
+
+				        <md-field>
+					      <label>Teléfono</label>
+					      <md-input v-model="phone"></md-input>
+					    </md-field>
+
+			        </md-list-item>
+			    </md-list>
+			    <md-list class="md-double-line">
+			    	<md-subheader>Tipo de Negocio</md-subheader>
+		    			<md-radio v-model="radio" class="radioOp" value="1">Puesto</md-radio>
+		    			<md-radio v-model="radio" class="radioOp" value="2">Local</md-radio>
+		    			<md-radio v-model="radio" class="radioOp" value="3">Restaurante</md-radio>
+			    </md-list>
+			    <md-button class="md-raised md-primary" @click="setDone('first', 'second')">Continue</md-button>
+			</div>
+		 		
+      		</md-step>
+      		<md-step id="second" md-label="Second Step" :md-error="secondStepError" :md-editable="false" :md-done.sync="second">
+      			<div class="viewport">
+					<md-toolbar :md-elevation="1">
+				      <span class="md-title">Amenidades</span>
+				    </md-toolbar>
+				    <md-list class="md-double-line">
+				    	<md-list-item v-for="ame in amenidades">
+						    <md-checkbox v-model="selectedAmen"  v-bind:value="ame.id">{{ame.descripcion}}</md-checkbox>
+				        </md-list-item>
+				    </md-list>
+				    <md-button class="md-raised md-primary" @click="setDone('second', 'third')">Continue</md-button>
+        			<md-button class="md-raised md-primary" @click="setError()">Set error!</md-button>
+				</div>
+      		</md-step>
+      		<md-step id="third" md-label="Third Step" :md-editable="false" :md-done.sync="third">
+      			<div class="viewport">
+					<md-toolbar :md-elevation="1">
+				      <span class="md-title">Redes Sociales</span>
+				    </md-toolbar>
+				    <md-list class="md-double-line">
+				    	<md-subheader>Facebook</md-subheader>
+				    	<md-list-item>
+						    <md-icon class="md-primary">thumb_up</md-icon>
+					        <md-field>
+						      <label>Facebook</label>
+						      <md-input v-model="facebook"></md-input>
+						    </md-field>
+				        </md-list-item>
+				        <md-list-item>
+						    <md-icon class="md-primary">camera_alt</md-icon>
+					        <md-field>
+						      <label>Instagram</label>
+						      <md-input v-model="instagram"></md-input>
+						    </md-field>
+				        </md-list-item>
+				        <md-list-item>
+						    <md-icon class="md-primary">public</md-icon>
+					        <md-field>
+						      <label>Sitio Web</label>
+						      <md-input v-model="website"></md-input>
+						    </md-field>
+				        </md-list-item>
+				   </md-list>
+				   <md-button class="md-raised md-primary" @click="setDone('third')">Done</md-button>
+				</div>
+      		</md-step>
+		 </md-steppers>
 		<form>
-			<md-field>
-		      <label>Nombre</label>
-		      <md-input v-model="initial"></md-input>
-		    </md-field>
-		    <md-field>
-		      <label>Type here!</label>
-		      <md-input v-model="type"></md-input>
-		      <span class="md-helper-text">Helper text</span>
-		    </md-field>
+			
+			
 		</form>
 	</div>
 </template>
 <script>
+	import axios from 'axios'
+	const URL = "http://localhost:8080/tacofy/amenidades"
 	export default{
 		name:'new-element',
 		data: () => ({
-			initial: 'Initial Value',
-      		type: null,
-		})
+			name: 'Taquería',
+      		phone: '55-65-98569',
+      		radio: false,
+      		amenidades:[],
+      		selectedAmen:[],
+      		facebook:'',
+      		instagram:'',
+      		website:'www.tacofy.com',
+      		active: 'first',
+	        first: false,
+	        second: false,
+	        third: false,
+	        secondStepError: null
+		}),
+		methods: {
+	      setDone (id, index) {
+	        this[id] = true
+
+	        this.secondStepError = null
+
+	        if (index) {
+	          this.active = index
+	        }
+	      },
+	      setError () {
+	        this.secondStepError = 'This is an error!'
+	      }
+	    },
+		mounted: function(){
+		    axios.get(URL).then(response => {
+		    	this.amenidades = response.data	
+		    })
+		  }
 	}
 </script>
+<style lang="scss" scoped>
+  .viewport {
+    width: 500px;
+    max-width: 100%;
+    display: inline-block;
+    vertical-align: top;
+    overflow: auto;
+    border: 1px solid rgba(#000, .12);
+  }
+  .radioOp{
+  	padding-left: 30px;
+  }
+</style>
